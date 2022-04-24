0c07d7eb126f7dd635e189a4adff48cecd4f1845
tommiechampion <94225105+tommiechampion@users.noreply.github.com>
2022-04-24 18:31:19 +0200

    Add files via upload
    Latest commit

---
+++ dummyData.js
@@ -0,0 +1,54 @@
+
+  export  const userData = [
+        {
+          name: 'Jan',
+          "Active User": 2000,
+         
+        },
+        {
+          name: 'Feb',
+          "Active User": 4000,
+         
+        }, {
+          name: 'Mar',
+          "Active User": 3000,
+         
+        }, {
+          name: 'Aor',
+          "Active User": 1000,
+         
+        }, {
+          name: 'May',
+          "Active User": 3000,
+         
+        }, {
+          name: 'Jun',
+          "Active User": 2000,
+         
+        }, {
+          name: 'Jul',
+          "Active User": 1000,
+         
+        }, {
+          name: 'Aug',
+          "Active User": 6000,
+         
+        }, {
+          name: 'Sep',
+          "Active User": 3000,
+         
+        }, {
+          name: 'Oct',
+          "Active User": 1000,
+         
+        }, {
+          name: 'Nov',
+          "Active User": 2000,
+         
+        }, {
+          name: 'Dec',
+          "Active User": 3000,
+         
+        }, 
+      
+      ];
